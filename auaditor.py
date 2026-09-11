e593e3e76ef1df60ce3d8e9d49c5e357a8a4faa0
Muhammad Afiq Danial Putera Kamsani <2604447@sit.singaporetech.edu.sg>
2026-09-11 11:31:29 +0800

    Manage state

--- auaditor.py
+++
@@ -1,7 +0,0 @@
-inv = []
-quit = 0
-
-while quit == 0:
-    info = input("Please enter stock quantity:")
-    if info == "quit":
-        break
--- auditor.py
+++ auditor.py
@@ -5,9 +5,19 @@ while quit == 0:
     info = input("Please enter stock quantity:")
     if info == "quit":
         print ("Thank you for updating")
+        print ("Here is the total", total)
         break
     elif (info.isdigit()) == True:
-        new_info = int(info)    
+        new_info = int(info)
+        if new_info < 0:                                                #  Omit - values
+            print (new_info)
+            print ("Try again")
+            failed +=1                                               # Failed/rejected inputs
+        else:                                                        #  Manage state [Update the inv list]
+            inv.append(new_info)
+            print (inv)                                              #  Show user list
+            int_inv = list(map(int, inv)) 
+            total = sum(int_inv)                                     
         print ("Accepted")     
     else:
         print ("Rejected/invalid Input")                              
