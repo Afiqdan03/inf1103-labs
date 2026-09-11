0b7929fc77e701e9b6cc279a0bbfb62513fb5e66
Muhammad Afiq Danial Putera Kamsani <2604447@sit.singaporetech.edu.sg>
2026-09-11 11:22:28 +0800

    Handle invalid inputs + Accept Stock Values

--- auditor.py
+++ auditor.py
@@ -4,4 +4,10 @@ quit = 0
 while quit == 0:
     info = input("Please enter stock quantity:")
     if info == "quit":
+        print ("Thank you for updating")
         break
+    elif (info.isdigit()) == True:
+        new_info = int(info)    
+        print ("Accepted")     
+    else:
+        print ("Rejected/invalid Input")                              
